53cbad595a429c67cfeed852745201d7bc670002
Wojciech Sabat <wojsabat@gmail.com>
2017-07-01 16:20:03 +0200

    added RelayCommand, implemented DeleteCommand

---
+++ WpfApp3/Commands/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp3.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        Action _TargetExecuteMethod;
+        Func<bool> _TargetCanExecuteMethod;
+
+        public RelayCommand(Action executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public RelayCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (_TargetCanExecuteMethod != null)
+            {
+                return _TargetCanExecuteMethod();
+            }
+            return _TargetExecuteMethod != null;
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter)
+        {
+            _TargetExecuteMethod?.Invoke();
+        }
+    }
+}
--- WpfApp3/ViewModels/ContactsViewModel.cs
+++ WpfApp3/ViewModels/ContactsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using WpfApp3.Commands;
 using WpfApp3.Model;
 using WpfApp3.Services;
 
@@ -8,8 +9,10 @@ namespace WpfApp3.ViewModels
 {
     public class ContactsViewModel : INotifyPropertyChanged
     {
-        private ContactsRepository _contactsRepository;
+        private readonly IContactsRepository _contactsRepository;
         private ObservableCollection<Contact> _contactsCollection;
+        private Contact _selectedContact;
+        private RelayCommand _deleteCommand;
 
         public ContactsViewModel()
         {
@@ -26,6 +29,34 @@ namespace WpfApp3.ViewModels
                 OnPropertyChanged();
             }
         }
+
+        public Contact SelectedContact
+        {
+            get => _selectedContact;
+            set
+            {
+                _selectedContact = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private async void DeleteExecute()
+        {
+            await _contactsRepository.DeleteAsync(_selectedContact.Id);
+            UpdateContactsCollection();
+        }
+
+        private void UpdateContactsCollection()
+        {
+            ContactsCollection = new ObservableCollection<Contact>(_contactsRepository.GetAllAsync().Result);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedContact != null;
+        }
+
+        public RelayCommand DeleteCommand => _deleteCommand ?? (_deleteCommand = new RelayCommand(DeleteExecute, CanDelete));
 
         public event PropertyChangedEventHandler PropertyChanged;
 
