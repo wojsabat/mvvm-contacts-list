a3248f16a38a7987bcf3cc071422298667235d0e
Wojciech Sabat <wojsabat@gmail.com>
2017-07-01 10:52:33 +0200

    added contactsViewModel

---
+++ WpfApp3/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfApp3.Model;
+using WpfApp3.Services;
+
+namespace WpfApp3.ViewModels
+{
+    public class ContactsViewModel : INotifyPropertyChanged
+    {
+        private ContactsRepository _contactsRepository;
+        private ObservableCollection<Contact> _contactsCollection;
+
+        public ContactsViewModel()
+        {
+            _contactsRepository = new ContactsRepository();
+            _contactsCollection = new ObservableCollection<Contact>(_contactsRepository.GetAllAsync().Result);
+        }
+
+        public ObservableCollection<Contact> ContactsCollection
+        {
+            get => _contactsCollection;
+            set
+            {
+                _contactsCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+}
