2f21c0cc891c96738ad267e770fa4fd7608f27be
Wojciech Sabat <wojsabat@gmail.com>
2017-07-01 10:14:08 +0200

    added Contact model

---
+++ WpfApp3/Model/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WpfApp3.Model
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+    }
+}
