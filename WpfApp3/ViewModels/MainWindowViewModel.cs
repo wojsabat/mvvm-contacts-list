6941f5a82a849097040f5ee859728efdcb226afa
Wojciech Sabat <wojsabat@gmail.com>
2017-07-01 20:32:33 +0200

    added CurrentViewModel property to main viewModel

--- WpfApp3/ViewModels/MainWindowViewModel.cs
+++ WpfApp3/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace WpfApp3.ViewModels
+﻿using System.Configuration;
+
+namespace WpfApp3.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
         private ContactsViewModel _contactsViewModel = new ContactsViewModel();
+        private BindableBase _currentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => SetProperty(ref _currentViewModel, value);
+        }
     }
 }
