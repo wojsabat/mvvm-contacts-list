e3292136132a56944c40f7f0ff1939cc75fe5513
Wojciech Sabat <wojsabat@gmail.com>
2017-07-01 10:22:05 +0200

    added ContactsContext

---
+++ WpfApp3/DAL/ContactsContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using WpfApp3.Model;
+
+namespace WpfApp3.DAL
+{
+    public class ContactsContext : DbContext
+    {
+        private DbSet<Contact> Contacts { get; set; }
+    }
+}
