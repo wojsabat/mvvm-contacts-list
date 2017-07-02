821c694d1637a6f333091450aecbde8818ede127
Wojciech Sabat <wojsabat@gmail.com>
2017-07-02 13:59:49 +0200

    added number validation

---
+++ MvvmContactList/IntegerTextBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+
+namespace MvvmContactList
+{
+    public class IntegerTextBox : TextBox
+    {
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+
+            Text = new string(Text.Where(char.IsDigit).ToArray());
+            SelectionStart = Text.Length;
+        }
+    }
+}
